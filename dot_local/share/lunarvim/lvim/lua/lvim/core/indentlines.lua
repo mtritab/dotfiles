16709862bc496061584c4ee6c7cba4d1058bdb05
Mike Tritabaugh <mtritab@gmail.com>
2023-05-19 12:44:43 -0500

    Update .local/share/lunarvim/lvim/lua/lvim/core/indentlines.lua

--- dot_local/share/lunarvim/lvim/lua/lvim/core/indentlines.lua
+++ dot_local/share/lunarvim/lvim/lua/lvim/core/indentlines.lua
@@ -11,23 +11,24 @@ M.config = function()
         "help",
         "startify",
         "dashboard",
-        "packer",
+        "lazy",
         "neogitstatus",
         "NvimTree",
         "Trouble",
         "text",
       },
       char = lvim.icons.ui.LineLeft,
+      context_char = lvim.icons.ui.LineLeft,
       show_trailing_blankline_indent = false,
       show_first_indent_level = true,
-      use_treesitter = false,
-      show_current_context = false,
+      use_treesitter = true,
+      show_current_context = true,
     },
   }
 end
 
 M.setup = function()
-  local status_ok, indent_blankline = pcall(reload, "indent_blankline")
+  local status_ok, indent_blankline = pcall(require, "indent_blankline")
   if not status_ok then
     return
   end
